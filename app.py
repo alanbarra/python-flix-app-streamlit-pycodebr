bf197303e1dc4c03553b79c84cb00e7670f2072a
unknown <alan.ghb@gmail.com>
2025-11-22 18:49:09 -0300

    carga

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from home.page import show_home
+from genres.page import show_genres
+from actors.page import show_actors
+from movies.page import show_movies
+from reviews.page import show_reviews
+from login.page import show_login
+
+
+def main():
+
+    if 'token' not in st.session_state:
+        show_login()
+    else:
+        st.title('Flix App')
+
+        menu_options = st.sidebar.selectbox(
+            'Selecione uma opção',
+            ['Início', 'Gênero', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+        )
+
+        if menu_options == 'Início':
+            show_home()
+
+        if menu_options == 'Gênero':
+            show_genres()
+
+        if menu_options == 'Atores/Atrizes':
+            show_actors()
+
+        if menu_options == 'Filmes':
+            show_movies()
+
+        if menu_options == 'Avaliações':
+            show_reviews()
+
+
+if __name__ == '__main__':
+    main()
